56be669fe9625d68630e8d4122eb8128347b5c6c
Naomi <naomirosenberguk@gmail.com>
2018-10-01 17:55:41 +0100

    Fixed the jQuery on the download data button
    The click binding was getting lost after ajax reload

--- modules/opencase_reporting/js/opencase-reporting.js
+++ modules/opencase_reporting/js/opencase-reporting.js
@@ -1,3 +1,5 @@
-jQuery('#opencase_reporting_buttons #download_data').click(function() {
-    location.href = location.href + "&_format=csv";
+jQuery('body').on('click','#opencase_reporting_buttons #download_data',function(){
+  var url = new URL(location.href);
+  url.searchParams.append('_format', 'csv');
+  location.href = url.toString();
 });
