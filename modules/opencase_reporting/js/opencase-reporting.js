b62f2f34a924ec87061766add4c0e2e72fbf35af
Naomi <naomirosenberguk@gmail.com>
2018-10-24 16:15:51 +0100

    Bodged download button for reporting.

--- modules/opencase_reporting/js/opencase-reporting.js
+++
@@ -1,5 +0,0 @@
-jQuery('body').on('click','#opencase_reporting_buttons #download_data',function(){
-  var url = new URL(location.href);
-  url.searchParams.append('_format', 'csv');
-  location.href = url.toString();
-});
