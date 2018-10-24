10b99f132e6956a586e66a84eecd2a29c03b68f9
Naomi <naomirosenberguk@gmail.com>
2018-10-24 17:40:26 +0100

    Dynamic menu links for adding actors

---
+++ src/Plugin/Derivative/AddActorsMenuLink.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\opencase\Plugin\Derivative;
+
+use Drupal\Component\Plugin\Derivative\DeriverBase;
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Derivative class that provides the menu links adding various types of actors
+ */
+class AddActorsMenuLink extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * @var EntityTypeManagerInterface $entityTypeManager.
+   */
+
+  protected $entityTypeManager;
+  
+  /**
+   * Creates a AddActorsMenuLink instance.
+   *
+   * @param $base_plugin_id
+   * @param EntityTypeManagerInterface $entity_type_manager
+   */
+  public function __construct($base_plugin_id, EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+ /**
+   * {@inheritdoc}
+   */
+ public static function create(ContainerInterface $container, $base_plugin_id) {
+   return new static(
+    $base_plugin_id,
+    $container->get('entity_type.manager')
+   );
+ }
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $links = [];
+    $actorTypes = $this->entityTypeManager->getStorage('oc_actor_type')->loadMultiple();
+    foreach ($actorTypes as $id => $actorType) {
+      $links[$id] = [
+        'title' => "Add a ". $actorType->label(),
+        'route_name' => "entity.oc_actor.add_form",
+        'route_parameters' => ['oc_actor_type' => $actorType->id()]
+      ] + $base_plugin_definition;
+    }
+    return $links;
+  }
+}
---
+++ src/Plugin/Menu/AddActorsMenuLink.php
@@ -0,0 +1,9 @@
+<?php
+namespace Drupal\opencase\Plugin\Menu;
+ 
+use Drupal\Core\Menu\MenuLinkDefault;
+ 
+/**
+ * Represents a menu link for adding an actor of some type.
+ */
+class AddActorsMenuLink extends MenuLinkDefault {}
